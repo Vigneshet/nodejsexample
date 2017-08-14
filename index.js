eae6a7654d84d4038ef472d109913005d58db761
Vignesh Thiyagarajan <mailvigneshthiyagarajan@gmail.com>
2017-08-14 22:52:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var port = process.env.PORT || 8080;
 app.get('/',function(req,res){
 	res.send('<h1>Hello World</h1>');	
 });
 
-http.listen(5858,function(){
-	console.log('server up in 5858')
+http.listen(port,function(){
+	console.log('server up');
 });
