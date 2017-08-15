de11b3e824757a6470b1d19e40ba842cebc3be23
Vignesh Thiyagarajan <mailvigneshthiyagarajan@gmail.com>
2017-08-15 10:03:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
+
 app.get('/',function(req,res){
-	res.send('<h1>Hello World</h1>');	
+	//res.send('<h1>Hello World</h1>');	
+	res.sendFile(__dirname+'/index.html');
 });
+
+io.on('connection',function(socket){
+	
+	console.log('user connected');
+})
+
 
 http.listen(port,function(){
 	console.log('server up');
