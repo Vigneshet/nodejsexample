062d1723ebae3114f4274fc21ab88154b881fb8f
Vignesh Thiyagarajan <mailvigneshthiyagarajan@gmail.com>
2017-08-26 21:59:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
+var socketMap = new Map();
+var socketName = new Map();
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
 	//res.send('<h1>Hello World</h1>');	
@@ -13,19 +16,44 @@ app.get('/',function(req,res){
 
 io.on('connection',function(socket){
 	console.log('user connected');
+	socket.on('join',function(data){
+		console.log('joining');
+		console.log(data.mailid);
+		console.log(data.username);
 		console.log(socket.id);
+		socketMap.set(data.mailid,socket);
+		socketName.set(data.mailid,data.username);
+	});
+	
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-	socket.on('chat message',function(msg){
-		console.log(msg);
-		var toSocketId = msg.split("~")[0];
-		var msgData = msg.split("~")[1];
-		io.to(toSocketId).emit('chat reply',msgData);
+	socket.on('chat message',function(data){
+		//console.log(msg);
+		var toSocketId = data.tosktId;
+		var msgData = data.msg;
+		var fromSocketId = data.fromsktId
+		socketMap.get(toSocketId).emit('chat reply',{
+			msgData : msgData,
+			fromSocketId:fromSocketId
+		});
 	});
 	
 });
 
+
+app.get('/userDetails',function(req,res){
+	var data ='';
+	console.log('/userdetails');
+	for (var [key, value] of socketName.entries()) {
+		console.log(key + ' = ' + value);
+		data +=key+"~"+value;
+		data += "#";
+	}
+	console.log(data);
+	//res.write(data);
+	res.end(data);
+})
 
 http.listen(port,function(){
 	console.log('server up');
